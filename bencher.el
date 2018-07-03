85d5c11d7c57127d7e78c2a6abdf354c0bcbcca5
Joel Svensson <svenssonjoel@yahoo.se>
2018-07-03 08:52:14 +0200

    Tweaks

--- bencher.el
+++ bencher.el
@@ -1,9 +1,31 @@
 
+;; This file is part of Emacs-Bencher.
+
+;; Emacs-Bencher is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; Emacs-Bencher is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
 ;; Joel Svensson 2018
 
 (require 'seq)
 (require 'cl)
 
+(defconst emacs-bencher-version '(0 0 0))
+(defun emacs-bencher-version-string ()
+  "Returns version string"
+  (let ((a (car emacs-bencher-version))
+	(b (car (cdr emacs-bencher-version)))
+	(c (car (cdr (cdr emacs-bencher-version)))))
+    (format "%s.%s.%s" a b c)))
 
 ;; Benchmark struct
 (cl-defstruct benchmark name executable varying tags)
